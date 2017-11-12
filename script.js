7e1bd63bc9407bfaf2af0b413cc766861f61bcc0
Joe Wales <joewales13@gmail.com>
2017-11-12 17:58:31 -0600

    completed assignment

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(readyNow);
+
+function readyNow(){
+   $('#submitButton').on('click', submitInformationClick);
+};
+
+var currentMonthlyCostTotal = 0;
+
+function Employee(first, last, id, title, salary) {
+    this.firstName = first;
+    this.lastName = last;
+    this.idNumber = id;
+    this.jobTitle = title;
+    this.annualSalary = salary;
+    this.employeeMonthlyCost = Math.round(this.annualSalary / 12);
+
+};
+
+
+function submitInformationClick () {
+    Employee.firstName = $('#firstName').val();
+    Employee.lastName = $('#lastName').val();
+    Employee.idNumber = $('#idNumber').val();
+    Employee.jobTitle = $('#jobTitle').val();
+    Employee.annualSalary = $('#annualSalary').val();
+    Employee.employeeMonthlyCost = Math.round(Employee.annualSalary / 12);
+
+    currentMonthlyCostTotal += Employee.employeeMonthlyCost;
+    document.getElementById('calcTotal').innerHTML = '$' + currentMonthlyCostTotal;
+    document.getElementById('inputForm').reset();
+    
+    $('tbody').append('<tr><td>' +
+        Employee.firstName + '</td>' +
+        '<td>' + Employee.lastName + '</td>' +
+        '<td>' + Employee.idNumber + '</td>' +
+        '<td>' + Employee.jobTitle + '</td>' +
+        '<td>' + Employee.annualSalary + '</td></tr>');
+    
+}; 
+
+    
+    
